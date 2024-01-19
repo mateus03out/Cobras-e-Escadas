8ca59d5adb8aefe01236af03aae2f1ab4f4a3be3
Araujo Mateus <mateus30outdoor@gmail.com>
2024-01-19 10:26:32 -0300

    Aumentando o espaco da matriz

--- CobrasEscadas.cpp
+++ CobrasEscadas.cpp
@@ -3,56 +3,78 @@
 
 using namespace std;
 
-#define rows 20
-#define col 21
+#define rows 22
+#define col 52
 
 struct posicaojogador{
 	int x,y;
 };
 
-void defEspaco(char espaco[][col]);
-struct posicaojogador posicao_inicial_jogador(int inicio);
+void def_espaco(char espaco[][col]);
+struct posicaojogador posicao_inicial_jogador();
+void def_cobras_escadas(char espaco[][col]);
+
 int main(){
 	char espaco[rows][col];
 	struct posicaojogador jogador[4];
 
 	//Posicao inicial dos jogadores
-	jogador[0] = posicao_inicial_jogador(0);
-	jogador[1] = posicao_inicial_jogador(0);
-	jogador[2] = posicao_inicial_jogador(0);
-	jogador[3] = posicao_inicial_jogador(0);
-	
+	jogador[0] = posicao_inicial_jogador();
+	jogador[1] = posicao_inicial_jogador();
+	jogador[2] = posicao_inicial_jogador();
+	jogador[3] = posicao_inicial_jogador();	
 	//limpando e definindo o espaco
-	defEspaco(espaco);
+	def_espaco(espaco);
+	def_cobras_escadas(espaco);
+	
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<col;j++)
+			if(espaco[i][j] == '0')
+				printf(" ");
+			else
+				printf("%c",espaco[i][j]);
+		printf("\n");
+	}
 	
 return 0;
 }
-void defEspaco(char espaco[][col]){
-	
-	for(int x=0;x<rows;x++)
-		for(int y=0;y<col;y++)
-			espaco[x][y] = '0';
-	for(int x=0;x<rows;x+=2)
-			for(int y=0;y<col;y++)
-				espaco[x][y] = '-';
-	for(int y=0;y<col;y+=2)
-		for(int x=0;x<rows;x++)
-			espaco[x][y] = '|';	
-	//Os proximos comandos podem ser removidos
-	for(int x=0;x<rows;x++){
-		for(int y=0;y<col;y++)
-			if(espaco[x][y] == '0')
-				cout << " ";
-			else
-				cout << espaco[x][y];		
-		printf("\n");
+void def_espaco(char espaco[][col]){
+	int codeASCII = 49;
+
+	//'Limpando' a matriz
+	for(int i=0;i<rows;i++)
+		for(int j=0;j<col;j++)
+			espaco[i][j] = '0';
+	//Definindo os espacos
+	for(int i=1;i<rows;i+=2)
+		for(int j=0;j<col;j++)
+			espaco[i][j] = '-';
+	for(int i=1;i<rows;i++)
+		for(int j=1;j<col;j+=5)
+			espaco[i][j] = '|';
+	//Numerando as linhas
+	for(int i=20;i>2;i-=2){
+		espaco[i][0] = codeASCII;
+		codeASCII++;
 	}
+	espaco[2][0] = 'D';
+	//Numerando as colunas
+	codeASCII = 49;
+	for(int j=47;j>2;j-=5){
+		espaco[0][j] = codeASCII;
+		codeASCII++;
+	}
+	espaco[0][2] = 'D';
 }
-struct posicaojogador posicao_inicial_jogador(int inicio){
+struct posicaojogador posicao_inicial_jogador(){
 	struct posicaojogador jogador;
 	
-		jogador.x = inicio;
-		jogador.y = inicio;
+		jogador.x = 21;
+		jogador.y = 2;
 
 	return jogador;
+}
+void def_cobras_escadas(char espaco[][col]){
+	//Escada 3 para 37
+	espaco[20][12] = 'E';
 }
