08c42e1877f2985dfcc4fce8c141d41639c9a425
Araujo Mateus <mateus30outdoor@gmail.com>
2024-01-26 22:20:10 -0300

    Uma versao mais funcional, proximo do fim.

--- CobrasEscadas.cpp
+++ CobrasEscadas.cpp
@@ -12,9 +12,10 @@ struct coordenadas{
 	int x,y;
 };
 void def_espaco(char espaco[][col]);
-void posicao_inicial_jogador(coordenadas jogador[4]);
+void posicao_inicial_jogador(char espaco[][col],coordenadas jogador[]);
 void gera_cobras_escadas(char espaco[][col]);
 void mostra_matriz(char espaco[][col]);
+void game(char espaco[][col],coordenadas jogador[]);
 
 int main(){
 	
@@ -22,14 +23,17 @@ int main(){
 
 	char espaco[rows][col];
 	struct coordenadas jogador[4];
-
-	//Posicao inicial dos jogadores
-	posicao_inicial_jogador(jogador);
-	//limpando e definindo o espaco
+    
+    //limpando e definindo o espaco
 	def_espaco(espaco);
 	//gera escadas e cobras
 	gera_cobras_escadas(espaco);
-	//mostra a matriz espaco
+	//Posicao inicial dos jogadores
+	posicao_inicial_jogador(espaco,jogador);
+	
+    //comeca o game
+    game(espaco, jogador);
+    //mostra a matriz espaco
 	mostra_matriz(espaco);
 	
 return 0;
@@ -48,19 +52,6 @@ void def_espaco(char espaco[][col]){
 	for(int i=1;i<rows;i++)
 		for(int j=1;j<col;j+=5)
 			espaco[i][j] = '|';
-	//Numerando as linhas
-	for(int i=20;i>2;i-=2){
-		espaco[i][0] = codeASCII;
-		codeASCII++;
-	}
-	espaco[2][0] = 'D';
-	//Numerando as colunas
-	codeASCII = 49;
-	for(int j=47;j>2;j-=5){
-		espaco[0][j] = codeASCII;
-		codeASCII++;
-	}
-	espaco[0][2] = 'D';
 	//Numerando as casas
 	for(int i=2;i<rows;i+=2){
 		codeASCII = 48;
@@ -76,38 +67,55 @@ void def_espaco(char espaco[][col]){
 		codeASCII++;
 	}
 }
-void posicao_inicial_jogador(coordenadas jogador[4]){
-	
-	for(int i=0;i<4;i++){
-		jogador[i].x = 21;
-		jogador[i].y = 2;
-	}
-}
+
 void gera_cobras_escadas(char espaco[][col]){
-	struct coordenadas cobras[10], escadas[6];
+    //escada, 1 para 23
+    espaco[20][7] = 'E';
+    espaco[16][17] = 'E';
+    //escada, 10 para 21
+    espaco[18][2] = 'E';
+    espaco[16][7] = 'E';
+    //escada, 26 para 59
+    espaco[16][32] = 'E';
+    espaco[10][47] = 'E';
+    //escada, 43 para 87
+    espaco[12][17] = 'E';
+    espaco[4][37] = 'E';
+    //escada, 61 para 94
+    espaco[8][7] = 'E';
+    espaco[2][22] = 'E';
 
-	for(int i=0;i<10;i++){
-		do{
-			cobras[i].x = 2*(rand() % 10 + 1);
-			cobras[i].y = (rand() % 10+1)*5-3;
-		}while(cobras[i].x == cobras[i].y);
-
-}	for(int i=0;i<10;i++)
-		espaco[cobras[i].x][cobras[i].y] = 'E';
-	
+    //Cobra, 33 para 3
+    espaco[14][17] = 'C';
+    espaco[20][17] = 'C'; 
 }
-void mostra_matriz(char espaco[][col]){
-	int cont=0;
+void posicao_inicial_jogador(char espaco[][col],coordenadas jogador[]){
+        int quant;
+        do{
+            cout << "Quantas pessoas irao jogar o jogo?<max 4><min 2>";
+            cin >> quant;
+        }while(quant < 2 || quant > 4);
+
+		for(int i=0;i<quant;i++){
+            jogador[i].x = 21;
+            jogador[i].y = i+2;
+        }
+        int codeASCII = 49;
+        for(int i=0;i<quant;i++){
+            espaco[jogador[i].x][jogador[i].y] = codeASCII;
+            codeASCII++;
+        }
+}
+void game(char espaco[][col], coordenadas jogador[]){
+    
+}
+void mostra_matriz(char espaco[][col]){	
 	for(int i=0;i<rows;i++){
-		for(int j=0;j<col;j++){
+		for(int j=0;j<col;j++)
 			if(espaco[i][j] == 'M')
 				printf(" ");
 			else
 				printf("%c",espaco[i][j]);
-		if(espaco[i][j] == 'E')
-			cont++;
-		}
 		printf("\n");
 	}
-	printf("\n%d\n",cont);
 }
